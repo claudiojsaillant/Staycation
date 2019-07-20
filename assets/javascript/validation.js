a7f7e0062c3e734cdaa60f11ea1630cb2c6cf324
Osy odife <osyodife@gmail.com>
2019-07-20 14:08:46 -0400

    upload modified files

--- assets/javascript/validation.js
+++ assets/javascript/validation.js
@@ -6,10 +6,23 @@ $.validator.addMethod("lettersonly", function(value, element) {
 
 $("#comment_form").validate({
     rules:{
+        comment:{
+            required: true,
+            lettersonly:true
+
+        },
         name:{
             required: true,
             lettersonly:true
-        }
+        },
+        email: {
+            required: true,
+            email: true,
+        },
+        messages:{
+            required: "Please enter an email address",
+            email: "Please a valid email address."
+        },
         
     },
 
